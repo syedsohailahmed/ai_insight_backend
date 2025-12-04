1482e66a0dce2ecb4dda2697b97a4f69d83bd0ff
Sohail Ahmed <sohail.ahmed@macys.com>
2025-12-04 18:20:38 +0500

    handle cors

--- api/analyze-product.js
+++ api/analyze-product.js
@@ -6,7 +6,7 @@ await initPinecone();
 
 export default async function handler(req, res) {
 
-  res.setHeader("Access-Control-Allow-Origin", "mfchgdpmkaikidbppjajegpphpehainc");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 

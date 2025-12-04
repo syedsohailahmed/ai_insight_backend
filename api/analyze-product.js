9605b60c5b4ef99853bff9851afd4987ce86fc79
Sohail Ahmed <sohail.ahmed@macys.com>
2025-12-04 18:16:03 +0500

    handle cors

--- api/analyze-product.js
+++ api/analyze-product.js
@@ -5,6 +5,15 @@ import { upsertVector, querySimilar, initPinecone } from "../services/vectorServ
 await initPinecone();
 
 export default async function handler(req, res) {
+
+  res.setHeader("Access-Control-Allow-Origin", "mfchgdpmkaikidbppjajegpphpehainc");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
 
   try {
